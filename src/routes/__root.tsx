2e570f816df052f5b78bb055c957d562bfcd1b71
ELPITO <espoirpowerboy@gmail.com>
2026-08-29 07:48:22 +0000

    Add new website favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -100,7 +100,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     links: [
       { rel: "stylesheet", href: appCss },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
-      { rel: "preconnect", href: "https://fonts.googleapis.com" },
+      { rel: "icon", href: "/favicon.png", type: "image/png" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
         rel: "stylesheet",
